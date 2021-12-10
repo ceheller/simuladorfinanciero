1402490a5e6be66d44cb7edbdf99e37781999386
unknown <camiloheller@gmail.com>
2021-12-10 02:15:57 -0300

    simplified code with parameters part 2

--- script.js
+++ script.js
@@ -36,32 +36,32 @@ function showResult(param1) {
     let monthlyBudget2 = addExpenses();
     part1 = "Según nuestros cálculos, tus gastos mensuales ascienden a USD " + monthlyBudget2 + ". Teniendo ello presente, necesitarás ahorrar ";
     part2 = Math.round(((monthlyBudget * 12) / param1) * 100);
-    part3 = " dólares invertidos, para vivir sin trabajar."
     document.getElementById("result").innerHTML = part1 + part2 + part3;
     const User = { "rent": rentBudget, "education": educationBudget, "goingOut": goingOutBudget, "supermarket": supermarketBudget, "health": healthBudget };
     const UserJson = JSON.stringify(User);
-    localStorage.setItem("User", UserJson);    
+    localStorage.setItem("User", UserJson); 
+    return part2
 }
 
 $(document).ready(function() {
     $("#conservativeResult").click(function () {
+    part3 = " dólares, invertidos a una tasa del 3% en el mercado inmobiliario, para vivir sin trabajar."
     showResult(3);
     });
 
     $("#middleRiskResult").click(function () {
+    part3 = " dólares, invertidos a una tasa del 9% en el mercado bursátil, para vivir sin trabajar."
     showResult(9);
     });
 
     $("#agressiveResult").click(function () {
-        let monthlyBudget2 = addExpenses();
-        const User = { "rent": rentBudget, "education": educationBudget, "goingOut": goingOutBudget, "supermarket": supermarketBudget, "health": healthBudget };
-        const UserJson = JSON.stringify(User);
-        localStorage.setItem("User", UserJson);
-        btcNeeded = (((monthlyBudget * 12) / 25) * 100) / bitcoinPrice2;
-        btcNeeded2 = btcNeeded.toFixed(4);
-        document.getElementById("result").innerHTML = "Según nuestros cálculos, tus gastos mensuales ascienden a USD " + monthlyBudget2 + ". Teniendo ello presente, necesitarás ahorrar " + btcNeeded2 + " Bitcoins al precio actual de mercado de USD " + bitcoinPrice2 + ", los que de valorizarse según la predicción matemática de Satoshi te permitirán vivir sin trabajar.";
-    });
-
+    part3 = ", los que de valorizarse según la predicción matemática de Satoshi te permitirán vivir sin trabajar";
+    btcNeeded = showResult(25) / bitcoinPrice2;
+    btcNeeded2 = btcNeeded.toFixed(4);
+    part2 = btcNeeded2 + " Bitcoins al precio actual de mercado de USD " + bitcoinPrice2
+    document.getElementById("result").innerHTML = part1 + part2 + part3;
+});
+    
     bitcoinPrice2 = 0;
     getBtcData = async () => {
         fetch('https://min-api.cryptocompare.com/data/price?fsym=BTC&tsyms=USD')
